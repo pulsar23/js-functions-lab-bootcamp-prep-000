92d5be2722c3035431edbf56f8589145ffb02552
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-06-16 21:07:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // write your code below!
+
+var name = "you";
+var holiday = 'Independence Day';
+var days = 20;
+
+
+function happyHolidays(){
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(name){
+  return 'Happy holidays, ${name}!';
+}
+
+function happyHolidayTo(holiday, name){
+  return 'Happy ${holiday}, ${name}!';
+}
+
+function holidayCountdown(holiday, days){
+  return 'It\'s ${days} days until ${holiday}!';
+}
+
+var string1 = happyHolidaysTo(name);
+var string2 = happyHolidayTo(holiday,name);
+holiday = "Mother's Day";
+var string3 = holidayCountdown(holiday,days);
